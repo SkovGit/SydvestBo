d8e079eb5534ec98a1c57c0ae54957574b026275
SkovGit <32670857+Skov86@users.noreply.github.com>
2019-11-26 14:50:15 +0100

    test
    new

--- SydvestBo/Program.cs
+++ SydvestBo/Program.cs
@@ -9,7 +9,8 @@ namespace SydvestBo
             Console.WriteLine("Hello World!");
             Console.WriteLine("New Changes");
             Console.WriteLine("Daniel has made a change ᕦ( ၜ ͜  ၜ )ᕥ ");
-            Console.WriteLine("Another One")
+            Console.WriteLine("Another One");
+            Console.WriteLine("Mike was here!!");
         }
     }
 }
