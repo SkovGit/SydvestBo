1fe79a3c1a2574c04aec2870bae67d72fa0f8bd0
SkovGit <32670857+Skov86@users.noreply.github.com>
2019-11-26 13:07:13 +0100

    first commit

--- SydvestBo/Program.cs
+++ SydvestBo/Program.cs
@@ -7,6 +7,7 @@ namespace SydvestBo
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("New Changes");
         }
     }
 }
