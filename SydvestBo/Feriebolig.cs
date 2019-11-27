7126c52d7cd3ba06fd162cdf6908ceb480e231d4
SkovGit <32670857+Skov86@users.noreply.github.com>
2019-11-27 08:43:43 +0100

    Propperties til klasserne

--- SydvestBo/Feriebolig.cs
+++ SydvestBo/Feriebolig.cs
@@ -4,6 +4,15 @@ namespace SydvestBo
     // Klasse til at manipulere data fra Ferieboligtabellen i databasen
     public class Feriebolig
     {
+        public string Adresse { get; set; }
+        public int Postnr { get; set; }
+        public int UdlejerId { get; set; }
+        public DateTime OpretDato { get; set; }
+        public string BoligType { get; set; }
+        public int BoStatus { get; set; }
+        public int Kalender { get; set; }
+
+
         public Feriebolig()
         {
             //Tom constructor
--- SydvestBo/Kontrakt.cs
+++ SydvestBo/Kontrakt.cs
@@ -3,6 +3,12 @@ namespace SydvestBo
 {
     public class Kontrakt
     {
+        public int Id { get; set; }
+        public string Adresse { get; set; }
+        public int Postnr { get; set; }
+        public int UdlejerID { get; set; }
+        public string Kontrakttype { get; set; }
+
         public Kontrakt()
         {
         }
